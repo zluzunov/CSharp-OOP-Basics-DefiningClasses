70f34120b8eb1bade9f934ba0b66f98f4fde66da
zluzunov <zluzunov@gmail.com>
2017-06-28 21:40:16 +0300

    Problems 2,3 ready 100%;

--- Lab/BankAccount.cs
+++ Lab/BankAccount.cs
@@ -1,9 +1,47 @@
-﻿public class BankAccount
+﻿namespace Lab
 {
-    private int id;
-    private double balance;
+    using System;
 
-    public int ID { get; set; }
-    public double Balance { get; set; }
+    public class BankAccount
+    {
+        private double balance;
+        private int id;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Tha balance can not be negative!");
+                }
+                balance = value;
+            }
+        }
+
+        public void Deposit(Double amount)
+        {
+            this.Balance += amount;
+        }
+
+        public void Withdraw(Double amount)
+        {
+            this.Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return $"Account ID{this.id}, balance {this.balance:f2}";
+        }
+    }
+
 }
+
 
