21b691eb7d1b9b3f6fa9ffbff1c58f31322616ee
zluzunov <zluzunov@gmail.com>
2017-06-30 15:22:39 +0300

    Problem 5 solved;

---
+++ Lab/DateModifier.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+public class DateModifier
+{
+    private DateTime start;
+    private DateTime end;
+
+    public DateModifier(string date1, string date2)
+    {
+        int[] date = date1
+            .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        this.start = new DateTime(date[0], date[1], date[2]);
+
+        date = date2
+            .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        this.end = new DateTime(date[0], date[1], date[2]);
+
+        if (DateTime.Compare(this.start,this.end) > 0)
+        {
+            DateTime tmp = this.end;
+            this.end = this.start;
+            this.start = tmp;
+        }
+    }
+
+    public double GetDifference()
+    {
+        TimeSpan diffTimeSpan = this.end-this.start;
+
+        return diffTimeSpan.TotalDays;
+    }
+}
--- Lab/Program.cs
+++ Lab/Program.cs
@@ -6,30 +6,12 @@ public class Program
 {
     static void Main()
     {
-        MethodInfo oldestMemberMethod = typeof(Family).GetMethod("GetOldestMember");
-        MethodInfo addMemberMethod = typeof(Family).GetMethod("AddMember");
-        if (oldestMemberMethod == null || addMemberMethod == null)
-        {
-            throw new Exception();
-        }
+        string date1 = Console.ReadLine();
+        string date2 = Console.ReadLine();
 
-        OpinionPoll poll = new OpinionPoll();
+        DateModifier dates = new DateModifier(date1,date2);
 
-        int people = int.Parse(Console.ReadLine());
-
-        for (int i = 0; i < people; i++)
-        {
-            var input = Console.ReadLine().Split(new []{' '},StringSplitOptions.RemoveEmptyEntries);
-            Person person = new Person(input[0], int.Parse(input[1]));
-            poll.AddMember(person);
-        }
-
-        Person[] older = poll.GetPersonsOlderThan(30);
-
-        foreach (Person person in older)
-        {
-            person.Print();
-        }
+        Console.WriteLine(dates.GetDifference());
     }
 }
 
