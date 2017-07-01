21f08dc5a8a8eae3c7c6d89efede6eb85f898a22
zluzunov <zluzunov@gmail.com>
2017-07-01 15:14:45 +0300

    Add rectangles plan svg;

--- Homework01/Cars/Engine.cs
+++ Homework01/Cars/Engine.cs
@@ -2,16 +2,28 @@
 {
     public class Engine
     {
-        public int Speed { get; set; }
+        private const double DefaultDisplacement = 0.0;
+        private const double DefaultEfficiency = 0.0;
+        public string Model { get; set; }
         public int Power { get; set; }
         public double Displacement { get; set; }
         public double Efficiency { get; set; }
 
 
-        public Engine(int speed, int power)
+        public Engine(string model, int power, double displacement, double efficiency = DefaultEfficiency)
         {
-            Speed = speed;
+            Model = model;
             Power = power;
+            Displacement = displacement;
+            Efficiency = efficiency;
         }
+
+        public Engine(string model, int power)
+        : this(model, power, DefaultDisplacement)
+        { }
+
+        public Engine(string model, int power, double efficiency)
+            : this(model, power, DefaultDisplacement, efficiency)
+        { }
     }
 }
